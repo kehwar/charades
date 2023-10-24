e537aaaac574301d1f45eff77dc741dd1f6cb836
Kehwar <erickkwr@gmail.com>
2023-10-23 22:06:01 -0500

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -17,7 +17,7 @@ const extend = (() => {
         return configArray
 
         // Only keep the rules
-            .filter(config => config.rules != null);
+            .filter((config) => config.rules != null);
     };
 })();
 
@@ -39,63 +39,129 @@ export default antfu(
     {
         files: ["**/*.js", "**/*.ts", "**/*.tsx", "**/*.vue"],
         rules: {
-
             // Eslint
+            "arrow-parens": ["error", "always"],
             "camelcase": [
                 "error",
                 {
                     allow: ["^UNSAFE_", "^U_", "^_", "^__"],
-                    properties: "never",
-                    ignoreGlobals: true,
                     ignoreDestructuring: false,
+                    ignoreGlobals: true,
                     ignoreImports: false,
+                    properties: "never",
                 },
             ],
             "no-multi-assign": "error",
 
             // Lodash
-            "lodash/prop-shorthand": ["error", "never"],
-            "lodash/matches-prop-shorthand": ["error", "never"],
+            "lodash/chain-style": ["error", "explicit"],
             "lodash/chaining": ["error", "always", 2],
             "lodash/import-scope": ["error", "full"],
+            "lodash/matches-prop-shorthand": ["error", "never"],
             "lodash/prefer-lodash-method": "off",
+            "lodash/prefer-lodash-typecheck": "off",
+            "lodash/prop-shorthand": ["error", "never"],
 
             // Import
+            "import/newline-after-import": ["error", { considerComments: false } ],
             "import/order": [
                 "error",
                 {
-                    groups: ["builtin", "external", "internal", "unknown", "parent", "index", "sibling", "object", "type"],
                     alphabetize: {
-                        order: "asc",
                         caseInsensitive: true,
+                        order: "asc",
                     },
+                    groups: [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "unknown",
+                        "parent",
+                        "index",
+                        "sibling",
+                        "object",
+                        "type",
+                    ],
                 },
             ],
 
+            // Sonar
+            "sonarjs/prefer-immediate-return": "off",
+            "sonarjs/prefer-single-boolean-return": "off",
+
             // Style
             "style/array-bracket-newline": ["error", { multiline: true } ],
-            "style/array-bracket-spacing": ["error", "never", { singleValue: false, objectsInArrays: true, arraysInArrays: true } ],
+            "style/array-bracket-spacing": [
+                "error",
+                "never",
+                { arraysInArrays: true, objectsInArrays: true, singleValue: false },
+            ],
             "style/array-element-newline": ["error", "consistent"],
+            "style/func-call-spacing": "error",
+            "style/indent": [
+                "error",
+                4,
+                {
+                    ArrayExpression: 1,
+                    CallExpression: { arguments: 1 },
+                    FunctionDeclaration: { body: 1, parameters: 1 },
+                    FunctionExpression: { body: 1, parameters: 1 },
+                    ImportDeclaration: 1,
+                    MemberExpression: 1,
+                    ObjectExpression: 1,
+                    SwitchCase: 1,
+                    VariableDeclarator: 1,
+                    flatTernaryExpressions: false,
+                    ignoreComments: false,
+                    ignoredNodes: [
+                        "TemplateLiteral *",
+                        "JSXElement",
+                        "JSXElement > *",
+                        "JSXAttribute",
+                        "JSXIdentifier",
+                        "JSXNamespacedName",
+                        "JSXMemberExpression",
+                        "JSXSpreadAttribute",
+                        "JSXExpressionContainer",
+                        "JSXOpeningElement",
+                        "JSXClosingElement",
+                        "JSXFragment",
+                        "JSXOpeningFragment",
+                        "JSXClosingFragment",
+                        "JSXText",
+                        "JSXEmptyExpression",
+                        "JSXSpreadChild",
+                        "TSTypeParameterInstantiation",
+                        "FunctionExpression > .params[decorators.length > 0]",
+                        "FunctionExpression > .params > :matches(Decorator, :not(:first-child))",
+                        "ClassBody.body > PropertyDefinition[decorators.length > 0] > .key",
+                    ],
+                    offsetTernaryExpressions: false,
+                    outerIIFEBody: 1,
+                },
+            ],
             "style/lines-around-comment": [
                 "error",
                 {
-                    beforeBlockComment: true,
                     afterBlockComment: false,
-                    beforeLineComment: true,
+                    beforeBlockComment: true,
+
                     afterLineComment: false,
-                    allowBlockStart: true,
-                    allowObjectStart: true,
+                    beforeLineComment: true,
+
                     allowArrayStart: true,
+                    allowBlockStart: true,
                     allowClassStart: true,
                     allowEnumStart: true,
                     allowInterfaceStart: true,
                     allowModuleStart: true,
+                    allowObjectStart: true,
                     allowTypeStart: true,
                     applyDefaultIgnorePatterns: true,
                     ignorePattern: "^\\s*@.*$",
                 },
             ],
-            "style/func-call-spacing": "error",
+            "style/quotes": ["error", "double", { allowTemplateLiterals: true, avoidEscape: true } ],
             "style/semi": ["error", "always"],
 
             // Tailwind
@@ -110,40 +176,47 @@ export default antfu(
                 },
             ],
 
+            // Typescript
+            "ts/ban-ts-comment": [
+                "error",
+                {
+                    "ts-expect-error": false,
+                },
+            ],
+            "ts/consistent-type-definitions": "off",
+            "ts/no-redeclare": "off",
+            "ts/no-use-before-define": "off",
+
             // Unicorn
-            "unicorn/prefer-string-slice": "error",
-            "unicorn/prefer-module": "error",
-            "unicorn/prefer-date-now": "error",
-            "unicorn/numeric-separators-style": "error",
-            "unicorn/new-for-builtins": "error",
-            "unicorn/consistent-destructuring": "error",
-            "unicorn/no-useless-undefined": "error",
+            "unicorn/better-regex": ["error", { sortCharacterClasses: false } ],
             "unicorn/catch-error-name": "error",
-            "unicorn/prefer-optional-catch-binding": "error",
-            "unicorn/no-array-callback-reference": "error",
+            "unicorn/consistent-destructuring": "error",
             "unicorn/consistent-function-scoping": "error",
-            "unicorn/no-empty-file": "error",
+            "unicorn/explicit-length-check": "error",
             "unicorn/filename-case": ["error", { case: "kebabCase" } ],
-            "unicorn/prefer-ternary": "error",
-            "unicorn/no-lonely-if": "error",
-            "unicorn/switch-case-braces": ["error", "always"],
+            "unicorn/new-for-builtins": "error",
+            "unicorn/no-array-callback-reference": "error",
             "unicorn/no-array-push-push": "error",
             "unicorn/no-array-reduce": "error",
-            "unicorn/prefer-set-has": "error",
-            "unicorn/explicit-length-check": "error",
-            "unicorn/no-useless-fallback-in-spread": "error",
             "unicorn/no-await-expression-member": "error",
-            "unicorn/better-regex": ["error", { sortCharacterClasses: false } ],
+            "unicorn/no-empty-file": "error",
+            "unicorn/no-lonely-if": "error",
+            "unicorn/no-useless-fallback-in-spread": "error",
+            "unicorn/no-useless-undefined": "error",
+            "unicorn/numeric-separators-style": "error",
+            "unicorn/prefer-date-now": "error",
+            "unicorn/prefer-module": "error",
+            "unicorn/prefer-optional-catch-binding": "error",
+            "unicorn/prefer-set-has": "error",
+            "unicorn/prefer-string-slice": "error",
+            "unicorn/prefer-ternary": "error",
+            "unicorn/switch-case-braces": ["error", "always"],
 
             // Vue
-            "vue/component-tags-order": ["error", { order: ["script", "template", "style"] } ],
-            "vue/component-api-style": ["error", ["script-setup"] ],
-            "vue/block-lang": ["error", { script: { lang: "ts" } } ],
-            "vue/define-props-declaration": ["error", "type-based"],
-            "vue/define-emits-declaration": ["error", "type-based"],
             "vue/attributes-order": [
                 "error",
                 {
+                    alphabetical: true,
                     order: [
                         "DEFINITION",
                         "LIST_RENDERING",
@@ -157,9 +230,13 @@ export default antfu(
                         "EVENTS",
                         "CONTENT",
                     ],
-                    alphabetical: true,
                 },
             ],
+            "vue/block-lang": ["error", { script: { lang: "ts" } } ],
+            "vue/component-api-style": ["error", ["script-setup"] ],
+            "vue/component-tags-order": ["error", { order: ["script", "template", "style"] } ],
+            "vue/define-emits-declaration": ["error", "type-based"],
+            "vue/define-props-declaration": ["error", "type-based"],
         },
     },
 
@@ -167,7 +244,6 @@ export default antfu(
     {
         files: ["**/index.ts"],
         rules: {
-            "consistent-default-export-name/default-import-match-filename": "off",
             "import/order": "off",
         },
     },
@@ -177,6 +253,23 @@ export default antfu(
         files: ["**/*.test.ts"],
         rules: {
             "no-console": "off",
+            "test/consistent-test-it": ["error", { fn: "test" } ],
+        },
+    },
+
+    // ["*.js"]
+    {
+        files: ["**/*.js"],
+        rules: {
+            "unicorn/prefer-module": "off",
+        },
+    },
+
+    // ["eslint.config.js"]
+    {
+        files: ["eslint.config.js"],
+        rules: {
+            "sort-keys": ["error", "asc", { allowLineSeparatedGroups: true } ],
         },
     },
 );
