5d1d3ff3cd05adcfb1cac878ce1ea1862eed7c69
Kehwar <erickkwr@gmail.com>
2023-10-27 16:24:02 -0500

    feat: update default ui config

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,15 @@
 export default defineAppConfig({
+    /* Tailwind */
     ui: {
         primary: "lime",
         gray: "neutral",
+        card: {
+            base: "flex flex-col",
+            body: {
+                base: "h-full overflow-auto",
+            },
+        },
     },
+
+    /* Tailwind */
 });
