fad97763a3b5092344588cf0f6191a6dc29035c0
Kehwar <erickkwr@gmail.com>
2023-10-28 00:56:40 -0500

    feat: update style

--- pages/deck/[deck]/index.vue
+++ pages/deck/[deck]/index.vue
@@ -17,52 +17,73 @@ const cardHistory = ref<CardGuess[]>([]);
             <span class="text-3xl">
                 {{ deck.name }}</span>
         </template>
-        <div class="grid gap-2">
-            <UButton
-                size="xl"
-                @click="state = 'playing'"
-            >
-                Start
-            </UButton>
-            <UButton
-                size="xl"
-                @click="navigateTo({ name: 'deck-deck-edit', params: { deck: deck.slug } })"
-            >
-                Edit
-            </UButton>
-            <UButton v-if="deck.url != null" external :to="`/decks/${deck.url}.json`">
-                See definition
-            </UButton>
-            <UButton to="/">
-                Return
-            </UButton>
-            <GameRound
-                v-if="state === 'playing'"
-                v-model:state="state"
-                :cards="cards"
-                class="w-full"
-                :time="!isDevelopment ? 120 : 9999"
-                @round-end="(h) => cardHistory = h"
-            />
-            <UCard v-if="state === 'idle' && cardHistory.length > 0" class="mt-4">
-                <template #header>
-                    <span>Results</span>
-                </template>
-                <ul class="flex flex-wrap gap-2">
-                    <li
-                        v-for="(card, index) in cardHistory"
-                        :key="index"
-                        class="grow break-words rounded bg-gray-100 p-2 text-center text-xl"
-                        :class="{
-                            'text-green-500': card.correct === true,
-                            'text-red-500': card.correct === false,
-                            'text-gray-500': card.correct === null,
-                        }"
+        <UButton
+            icon="i-heroicons-play-circle"
+            size="xl"
+            @click="state = 'playing'"
+        >
+            {{ cardHistory.length === 0 ? 'Start' : 'Restart' }}
+        </UButton>
+        <UCard v-if="state === 'idle' && cardHistory.length > 0" class="mt-4">
+            <template #header>
+                <span>Results</span>
+            </template>
+            <ul class="flex flex-wrap gap-2">
+                <li
+                    v-for="(card, index) in cardHistory"
+                    :key="index"
+                    class="grow break-words rounded bg-gray-100 p-2 text-center text-xl"
+                    :class="{
+                        'text-green-500': card.correct === true,
+                        'text-red-500': card.correct === false,
+                        'text-gray-500': card.correct === null,
+                    }"
+                >
+                    {{ card.card }}
+                </li>
+            </ul>
+        </UCard>
+        <GameRound
+            v-if="state === 'playing'"
+            v-model:state="state"
+            :cards="cards"
+            class="w-full"
+            :time="!isDevelopment ? 120 : 9999"
+            @round-end="(h) => cardHistory = h"
+        />
+        <template #footer>
+            <div class="flex justify-between gap-2">
+                <div class="flex gap-2">
+                    <UButton
+                        icon="i-heroicons-play-circle"
+                        @click="state = 'playing'"
                     >
-                        {{ card.card }}
-                    </li>
-                </ul>
-            </UCard>
-        </div>
+                        Start
+                    </UButton>
+                    <UButton
+                        icon="i-heroicons-pencil-square"
+                        @click="navigateTo({ name: 'deck-deck-edit', params: { deck: deck.slug } })"
+                    >
+                        Edit
+                    </UButton>
+                </div>
+                <div class="flex gap-2">
+                    <UButton
+                        v-if="deck.url != null"
+                        external
+                        icon="i-heroicons-code-bracket"
+                        :to="`/decks/${deck.url}.json`"
+                    >
+                        Source
+                    </UButton>
+                    <UButton
+                        icon="i-heroicons-home"
+                        to="/"
+                    >
+                        Return
+                    </UButton>
+                </div>
+            </div>
+        </template>
     </UCard>
 </template>
